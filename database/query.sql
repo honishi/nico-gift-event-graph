d10039ef4f6aff3140c21a13b4402b89204b7085
Hiroyuki Onishi <onishi.hiroyuki@gmail.com>
2022-05-29 01:05:07 +0900

    update front page

--- database/query.sql
+++ database/query.sql
@@ -2,3 +2,7 @@ use nico_gift_event_graph_db;
 select * from ranking;
 show grants;
 truncate table ranking;
+select max(timestamp) from ranking where gift_event_id = '202206_camp';
+select distinct timestamp from ranking where gift_event_id = '202206_camp' order by timestamp asc;
+select item_id from ranking where gift_event_id = '202206_camp' and timestamp = 1653748382 order by `rank` asc limit 10;
+select total_score from ranking where gift_event_id = '202206_camp' and item_id = 54091817 order by timestamp desc;
--- web/main.py
+++ web/main.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import configparser
-import os
 import sys
+from datetime import datetime
+from typing import List
 
-import boto3
-from boto3.dynamodb.conditions import Key, Attr
+import mysql.connector
 from flask import render_template, Flask
 
 app = Flask(__name__)
@@ -14,25 +14,45 @@ app = Flask(__name__)
 class EventSetting:
     setting_name: str
     gift_event_id: str
-    aws_access_key_id: str
-    aws_secret_access_key: str
-    aws_region: str
+    db_host: str
+    db_port: int
+    db_user: str
+    db_password: str
 
-    def __init__(self, setting_name: str, gift_event_id: str, aws_access_key_id: str, aws_secret_access_key: str,
-                 aws_region: str):
+    def __init__(self, setting_name: str, gift_event_id: str, db_host: str, db_port: int, db_user: str,
+                 db_password: str):
         self.setting_name = setting_name
         self.gift_event_id = gift_event_id
-        self.aws_access_key_id = aws_access_key_id
-        self.aws_secret_access_key = aws_secret_access_key
-        self.aws_region = aws_region
+        self.db_host = db_host
+        self.db_port = db_port
+        self.db_user = db_user
+        self.db_password = db_password
+
+
+class RankUser:
+    name: str
+    scores: List[int]
+
+    def __init__(self, name: str, scores: List[int]):
+        self.name = name
+        self.scores = scores
+
+
+class RankingData:
+    labels: List[str]
+    top_users: List[RankUser]
+
+    def __init__(self, labels: List[str], top_users: List[RankUser]):
+        self.labels = labels
+        self.top_users = top_users
 
 
 @app.route("/")
 def top():
     event_setting = read_event_settings()
-    set_aws_environmental_values(event_setting)
-    load_from_dynamodb(event_setting)
-    return render_template('index.html')
+    data = make_ranking_data(event_setting)
+    print(data)
+    return render_template('index.html', data=data)
 
 
 def read_event_settings() -> EventSetting:
@@ -42,28 +62,88 @@ def read_event_settings() -> EventSetting:
     return EventSetting(
         section,
         config.get(section, "gift_event_id"),
-        config.get(section, "aws_access_key_id"),
-        config.get(section, "aws_secret_access_key"),
-        config.get(section, "aws_region")
+        config.get(section, "db_host"),
+        int(config.get(section, "db_port")),
+        config.get(section, "db_user"),
+        config.get(section, "db_password")
     )
 
 
-def set_aws_environmental_values(setting: EventSetting):
-    os.environ['AWS_ACCESS_KEY_ID'] = setting.aws_access_key_id
-    os.environ['AWS_SECRET_ACCESS_KEY'] = setting.aws_secret_access_key
-    os.environ['AWS_DEFAULT_REGION'] = setting.aws_region
-
-
-def load_from_dynamodb(setting: EventSetting):
-    dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('nico-gift-event-graph')
-    result = table.query(
-        KeyConditionExpression=Key('partition_key').eq(setting.gift_event_id),
-        # FilterExpression=Key('gift_event_id').eq(setting.gift_event_id),
-        # IndexName="partition_key-gift_event_id-index"
+def make_ranking_data(setting: EventSetting) -> RankingData:
+    connection = mysql.connector.connect(
+        host=setting.db_host,
+        port=setting.db_port,
+        user=setting.db_user,
+        password=setting.db_password,
+        database='nico_gift_event_graph_db'
     )
-    print(result)
+    cursor = connection.cursor()
 
+    # latest timestamp
+    sql = f"""
+    select max(timestamp) 
+    from ranking 
+    where gift_event_id = '{setting.gift_event_id}'
+    """
+    cursor.execute(sql)
+    row = cursor.fetchone()
+    latest_timestamp = row[0]
+
+    # timestamps
+    sql = f"""
+    select distinct timestamp 
+    from ranking 
+    where gift_event_id = '{setting.gift_event_id}'
+    order by timestamp asc
+    """
+    cursor.execute(sql)
+    rows = cursor.fetchall()
+    labels = []
+    for row in rows:
+        date = datetime.fromtimestamp(row[0])
+        label = date.strftime('%Y/%m/%d %H:%M:%S')
+        labels.append(label)
+    print(labels)
+
+    # top n user at lates timestamp
+    sql = f"""
+    select item_id, name
+    from ranking
+    where gift_event_id = '{setting.gift_event_id}' 
+    and timestamp = {latest_timestamp} 
+    order by `rank` asc limit 10
+    """
+    cursor.execute(sql)
+    rows = cursor.fetchall()
+    top_users = []
+    for row in rows:
+        top_users.append(row)
+    print(top_users)
+
+    # each user's score history
+    history = []
+    for user_id, name in top_users:
+        sql = f"""
+        select total_score 
+        from ranking 
+        where gift_event_id = '{setting.gift_event_id}'
+        and item_id = '{user_id}'
+        order by timestamp desc;
+        """
+        cursor.execute(sql)
+        rows = cursor.fetchall()
+        scores = []
+        for row in rows:
+            scores.append(row[0])
+        print(user_id, name)
+        print(scores)
+        history.append((name, scores))
+
+    # make data
+    return RankingData(
+        labels,
+        list((RankUser(name, list(reversed(scores))) for name, scores in history)),
+    )
 
 if __name__ == "__main__":
     if len(sys.argv) == 3:
