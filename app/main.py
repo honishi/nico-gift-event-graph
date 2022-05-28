334a3414a5bb5e60d6487f907aaedb204a88580d
Hiroyuki Onishi <onishi.hiroyuki@gmail.com>
2022-05-28 15:57:42 +0900

    add s3 upload support

--- app/main.py
+++ app/main.py
@@ -2,19 +2,32 @@ import configparser
 import datetime
 from typing import List, Optional
 
+import boto3
 import requests
 
+TMP_DIR = "./tmp"
 USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36"
 
 
 class EventSetting:
     setting_name: str
     ranking_json_url: str
+    s3_iam_access_key_id: str
+    s3_iam_secret_access_key: str
+    s3_region: str
+    s3_bucket: str
+    s3_folder: str
     save_file_prefix: str
 
-    def __init__(self, setting_name: str, ranking_json_url: str, save_file_prefix: str):
+    def __init__(self, setting_name: str, ranking_json_url: str, s3_iam_access_key_id: str,
+                 s3_iam_secret_access_key: str, s3_region: str, s3_bucket: str, s3_folder: str, save_file_prefix: str):
         self.setting_name = setting_name
         self.ranking_json_url = ranking_json_url
+        self.s3_iam_access_key_id = s3_iam_access_key_id
+        self.s3_iam_secret_access_key = s3_iam_secret_access_key
+        self.s3_region = s3_region
+        self.s3_bucket = s3_bucket
+        self.s3_folder = s3_folder
         self.save_file_prefix = save_file_prefix
 
 
@@ -31,7 +44,21 @@ class NicoGiftEventLoader:
             print(text)
             if text is None:
                 continue
-            NicoGiftEventLoader.save_ranking_json(text, event_setting.save_file_prefix, date)
+            filename = f"{event_setting.save_file_prefix}_{date.strftime('%Y%m%d%H%M%S')}_{int(date.timestamp())}.json"
+            NicoGiftEventLoader.save_ranking_json(
+                text,
+                TMP_DIR,
+                filename
+            )
+            NicoGiftEventLoader.backup_json_to_s3(
+                TMP_DIR,
+                filename,
+                event_setting.s3_iam_access_key_id,
+                event_setting.s3_iam_secret_access_key,
+                event_setting.s3_region,
+                event_setting.s3_bucket,
+                event_setting.s3_folder,
+            )
             # dynamodb
 
     @staticmethod
@@ -44,6 +71,11 @@ class NicoGiftEventLoader:
                 EventSetting(
                     section,
                     config.get(section, "ranking_json_url"),
+                    config.get(section, "s3_iam_access_key_id"),
+                    config.get(section, "s3_iam_secret_access_key"),
+                    config.get(section, "s3_region"),
+                    config.get(section, "s3_bucket"),
+                    config.get(section, "s3_folder"),
                     config.get(section, "save_file_prefix")
                 )
             )
@@ -55,11 +87,24 @@ class NicoGiftEventLoader:
         return response.text if response.status_code == 200 else None
 
     @staticmethod
-    def save_ranking_json(text: str, prefix: str, date: datetime):
-        filename = 'ranking_json/' + prefix + date.strftime('%Y%m%d_%H%M%S') + ".json"
-        file = open(filename, "wb")
-        file.write(text.encode('utf-8'))
-        file.close()
+    def save_ranking_json(text: str, directory: str, filename: str) -> Optional[str]:
+        fullpath = f"{directory}/{filename}"
+        with open(fullpath, "wb") as f:
+            f.write(text.encode('utf-8'))
+        return fullpath
+
+    @staticmethod
+    def backup_json_to_s3(directory: str, filename: str, s3_access_key_id: str, s3_secret_access_key: str,
+                          s3_region: str, s3_bucket: str, s3_folder: str):
+        client = boto3.client(
+            's3',
+            aws_access_key_id=s3_access_key_id,
+            aws_secret_access_key=s3_secret_access_key,
+            region_name=s3_region
+        )
+        local_file = f"{directory}/{filename}"
+        s3_object = f"{s3_folder}/{filename}"
+        client.upload_file(local_file, s3_bucket, s3_object)
 
     @staticmethod
     def make_http_header():
