c598b4980438ca6f9b5cc03cb5153dac5f15f840
Hiroyuki Onishi <onishi.hiroyuki@gmail.com>
2022-05-31 00:42:28 +0900

    adjust sql

--- database/query.sql
+++ database/query.sql
@@ -6,3 +6,4 @@ select max(timestamp) from ranking where gift_event_id = '202206_camp';
 select distinct timestamp from ranking where gift_event_id = '202206_camp' order by timestamp asc;
 select item_id from ranking where gift_event_id = '202206_camp' and timestamp = 1653748382 order by `rank` asc limit 10;
 select total_score from ranking where gift_event_id = '202206_camp' and item_id = 54091817 order by timestamp desc;
+select count(*) from ranking;
