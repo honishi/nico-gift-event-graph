ff20d2731b4ac4ac242510f1bb9932dbb48c6f02
Hiroyuki Onishi <onishi.hiroyuki@gmail.com>
2022-10-08 11:30:25 +0200

    add database index

--- database/create_db.sql
+++ database/create_db.sql
@@ -30,3 +30,16 @@ DROP USER 'nico_gift_event_graph_user';
 CREATE USER 'nico_gift_event_graph_user' IDENTIFIED BY 'xxx';
 GRANT ALL PRIVILEGES ON nico_gift_event_graph_db.* TO 'nico_gift_event_graph_user';
 FLUSH PRIVILEGES;
+
+--
+-- Create Index.
+--
+DROP INDEX ranking_index_gift_event_id_timestamp
+ON nico_gift_event_graph_db.ranking;
+CREATE INDEX ranking_index_gift_event_id_timestamp
+ON nico_gift_event_graph_db.ranking (gift_event_id, timestamp);
+
+DROP INDEX ranking_index_gift_event_id_item_id_timestamp_total_score
+ON nico_gift_event_graph_db.ranking;
+CREATE INDEX ranking_index_gift_event_id_item_id_timestamp_total_score
+ON nico_gift_event_graph_db.ranking (gift_event_id, item_id, timestamp desc, total_score);
--- database/query.sql
+++ database/query.sql
@@ -12,10 +12,36 @@ select * from ranking;
 select count(*) from ranking;
 truncate table ranking;
 
+
+--
+-- Explain Plan.
+--
+explain
+select distinct timestamp
+from ranking
+where gift_event_id = '202209-demae'
+order by timestamp desc;
+
+explain
+select item_id, name
+from ranking
+where gift_event_id = '202209-demae'
+  and timestamp = 1665042602
+order by `rank` asc
+limit 15;
+
+explain
+select timestamp, total_score
+from ranking
+where gift_event_id = '202209-demae'
+  and item_id = '52553742'
+order by timestamp desc;
+
 --
 -- Various Query.
 --
-select max(timestamp) from ranking where gift_event_id = '202206_camp';
+select max(timestamp) from ranking where gift_event_id = '202206-camp';
 select distinct timestamp from ranking where gift_event_id = '202206_camp' order by timestamp asc;
 select item_id from ranking where gift_event_id = '202206_camp' and timestamp = 1653748382 order by `rank` asc limit 10;
 select total_score from ranking where gift_event_id = '202206_camp' and item_id = 54091817 order by timestamp desc;
+select count(*) from ranking;
